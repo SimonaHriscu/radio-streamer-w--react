94cdb6d5290e246e60e38bee3af3873d5e67259e
Simona <see.moona@yahoo.com>
2020-09-10 14:30:30 +0200

    font-awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
 import App from './App';
+import './components/Icons/lib';
 
 ReactDOM.render(
   <React.StrictMode>

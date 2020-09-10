5a932e72699d363b624785bc58253884c4724925
Simona <see.moona@yahoo.com>
2020-09-10 12:54:13 +0200

    radio component

---
+++ src/components/Radio/radio-component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Layout/Header/header-component';
+import Main from '../Layout/Main/main-component';
+import Footer from '../Layout/Footer/footer-component';
+
+const Radio = () => {
+  return (
+    <div className="radio-component">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default Radio;

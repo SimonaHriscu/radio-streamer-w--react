fd221aa788f2780c023bc81ad460a0fb066f0f8e
Simona <see.moona@yahoo.com>
2020-09-10 11:46:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Radio from './components/radio.component';
+const App = () => {
+  return (
+    <div className="App">
+      <Radio />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/radio.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Radio = () => {
+  return <div className="radio-component"></div>;
+};
+
+export default Radio;

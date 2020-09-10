030df9b50b624ca8ffb1469944beaf0ffd269079
Simona <see.moona@yahoo.com>
2020-09-10 14:31:16 +0200

    header finished

--- src/components/Layout/Header/header-component.jsx
+++ src/components/Layout/Header/header-component.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
-  return <div className="header-component"></div>;
+  return (
+    <div className="header-component">
+      <FontAwesomeIcon icon="chevron-left" className="icon" />
+      <h1>stations</h1>
+      <FontAwesomeIcon icon="power-off" className="icon" />
+    </div>
+  );
 };
 
 export default Header;

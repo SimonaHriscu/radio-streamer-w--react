749d4dd4b74f505b42e747aef55642643e9da70f
Simona <see.moona@yahoo.com>
2020-09-10 12:54:35 +0200

    layout added

---
+++ src/components/Layout/Header/header-component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className="header-component"></div>;
+};
+
+export default Header;
---
+++ src/components/Layout/Main/main-component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => {
+  return <div className="main-component"></div>;
+};
+
+export default Main;

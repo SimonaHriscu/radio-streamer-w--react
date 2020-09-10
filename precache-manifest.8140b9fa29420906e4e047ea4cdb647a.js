9770c6681cbdb45524fded8b18323d193cd26748
Simona <see.moona@yahoo.com>
2020-09-10 19:08:59 +0200

    Updates

---
+++ precache-manifest.8140b9fa29420906e4e047ea4cdb647a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf54ebcd70809f914fe7b55b68050d7b",
+    "url": "/code-challenge-react/index.html"
+  },
+  {
+    "revision": "e97432081790e14e11b0",
+    "url": "/code-challenge-react/static/css/main.f403f0d8.chunk.css"
+  },
+  {
+    "revision": "eb332e60fe1ec213f69c",
+    "url": "/code-challenge-react/static/js/2.4e08c108.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-challenge-react/static/js/2.4e08c108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97432081790e14e11b0",
+    "url": "/code-challenge-react/static/js/main.4faba915.chunk.js"
+  },
+  {
+    "revision": "96eef120d0782935ac70",
+    "url": "/code-challenge-react/static/js/runtime-main.729d24c6.js"
+  }
+]);
